d565a91826e0e35104b7b83adf1746a00c71d870
jpang32 <jpangallozzi@gmail.com>
2022-04-16 21:55:59 -0400

    Initial Commit

---
+++ QuickSlider/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuickSlider
+//
+//  Created by Jared Pangallozzi on 4/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuickSlider/QuickSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickSliderApp.swift
+//  QuickSlider
+//
+//  Created by Jared Pangallozzi on 4/16/22.
+//
+
+import SwiftUI
+
+@main
+struct QuickSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
