f4e8f54c91d26c69a52701f127e8ef3dfb793e89
jpang32 <jpangallozzi@gmail.com>
2022-04-25 19:17:57 -0400

    Circle shows different colors based on pts zone. Added janky acceleration function

--- QuickSlider/Models/Game.swift
+++ QuickSlider/Models/Game.swift
@@ -9,10 +9,20 @@ import Foundation
 
 struct Game {
     
-    var target: Int = Int.random(in: 0...359)
+    var target: Double = Double(Int.random(in: 0...359))
+    // Unit: degrees / sec^2
+    var acceleration: Double = 30.0
     
-    func getSpeed(angle: Int) -> Double {
-        return 0.0
+    // Given as percentages of circle for .trim() function
+    var yellowRange: Double = 1.0 / 12.0
+    var redRange: Double = 1.0 / 20.0
+    
+    init() {
+        print(self.target)
+    }
+    
+    func getSpeed(currentAngle: Double) -> Double {
+        return (1.0 / abs(self.target - currentAngle.truncatingRemainder(dividingBy: 360)) * self.acceleration)
     }
     
     
--- QuickSlider/Views/QuickSliderView.swift
+++ QuickSlider/Views/QuickSliderView.swift
@@ -9,27 +9,48 @@ import SwiftUI
 
 struct QuickSliderView: View {
     
-    let timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
+    private let timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
     
-    let radius: CGFloat = 300
-    @State private var angle: Double = 0
+    private let radius: CGFloat = 300
+    private let game: Game = Game()
+    
+    @State private var angle: Double = 0.0
     
     var body: some View {
         
         
         VStack {    
             Circle()
+                //.trim(from: <#T##CGFloat#>, to: <#T##CGFloat#>)
                 .strokeBorder(Color.green, lineWidth: 2)
                 .frame(width: self.radius, height: self.radius)
+            /*
+                .mask(Circle()
+                    .trim(from: game.target - game.yellowRange, to: game.target + game.yellowRange)
+                    //.stroke(Color.yellow, lineWidth: 2)
+                )
+             */
+                .overlay(Circle()
+                    .trim(from: game.target / 360.0 - game.yellowRange,
+                          to: game.target / 360.0 + game.yellowRange)
+                    .stroke(Color.yellow, lineWidth:2)
+                    .frame(width: self.radius, height: self.radius)
+                )
+                .overlay(Circle()
+                    .trim(from: game.target / 360.0 - game.redRange,
+                          to: game.target / 360.0 + game.redRange)
+                    .stroke(Color.red, lineWidth:2)
+                    .frame(width: self.radius, height: self.radius)
+                )
                 .overlay(
                     Image(systemName: "arrow.down")
                         .offset(x: self.radius / 2)
                         .rotationEffect(.degrees(self.angle))
                 )
         }
-        .animation(.default, value: angle)
+        .animation(.default, value: self.angle)
         .onReceive(timer) { _ in
-            angle += 1
+            self.angle = self.angle + game.getSpeed(currentAngle: self.angle)
         }
          
             
