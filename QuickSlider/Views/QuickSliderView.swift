58e4552553c6a91e9684a209eb61686dac29ed09
jpang32 <jpangallozzi@gmail.com>
2022-04-29 22:53:56 -0400

    Fixed the bugs with coloring of circles

--- QuickSlider/Views/QuickSliderView.swift
+++ QuickSlider/Views/QuickSliderView.swift
@@ -9,45 +9,48 @@ import SwiftUI
 
 struct QuickSliderView: View {
     
-    private let timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
+    private var timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
+    @State private var timerOn: Bool = true
     
     private let radius: CGFloat = 300
+    @State private var angle: Double = 0.0
+    
     private let game: Game = Game()
     
-    @State private var angle: Double = 0.0
+    /*
+    private func toggle_timer(timerOn: timerOn) {
+        if self.timerOn {
+            self.timer.upstream.autoconnect()
+        } else {
+            self.timer.upstream.connect().cancel()
+        }
+    }]*/
     
     var body: some View {
         
         
         VStack {    
-            Circle()
-                //.trim(from: <#T##CGFloat#>, to: <#T##CGFloat#>)
-                .strokeBorder(Color.green, lineWidth: 2)
-                .frame(width: self.radius, height: self.radius)
-            /*
-                .mask(Circle()
-                    .trim(from: game.target - game.yellowRange, to: game.target + game.yellowRange)
-                    //.stroke(Color.yellow, lineWidth: 2)
-                )
-             */
-                .overlay(Circle()
-                    .trim(from: game.target / 360.0 - game.yellowRange,
-                          to: game.target / 360.0 + game.yellowRange)
-                    .stroke(Color.yellow, lineWidth:2)
+            ZStack {
+                Circle()
+                    //.trim(from: <#T##CGFloat#>, to: <#T##CGFloat#>)
+                    .strokeBorder(Color.green, lineWidth: 2)
                     .frame(width: self.radius, height: self.radius)
+                    .overlay(
+                        Image(systemName: "arrow.down")
+                            .offset(x: self.radius / 2)
+                            .rotationEffect(.degrees(self.angle))
                 )
-                //this is a test
-                .overlay(Circle()
-                    .trim(from: game.target / 360.0 - game.redRange,
-                          to: game.target / 360.0 + game.redRange)
-                    .stroke(Color.red, lineWidth:2)
-                    .frame(width: self.radius, height: self.radius)
-                )
-                .overlay(
-                    Image(systemName: "arrow.down")
-                        .offset(x: self.radius / 2)
-                        .rotationEffect(.degrees(self.angle))
-                )
+                Circle()
+                    .trim(from: 0.5 - game.yellowRange, to: 0.5 + game.yellowRange)
+                    .stroke(Color.yellow, lineWidth: 2)
+                    .frame(width: self.radius, height:self.radius)
+                    .rotationEffect(.degrees(game.target - 180.0))
+                Circle()
+                    .trim(from: 0.5 - game.redRange, to: 0.5 + game.redRange)
+                    .stroke(Color.red, lineWidth: 2)
+                    .frame(width: self.radius, height:self.radius)
+                    .rotationEffect(.degrees(game.target - 180.0))
+            }
         }
         .animation(.default, value: self.angle)
         .onReceive(timer) { _ in
